095454597e12183d5c6f832ee88251ceb2c2eb6a
Alexander Breus <Anyuta069@yandex.ru>
2024-05-11 21:44:11 +0300

    add get menu items

--- top-app-page-router/top-app/pages/index.tsx
+++ top-app-page-router/top-app/pages/index.tsx
@@ -1,17 +1,18 @@
 import { Button, Htag, P, Rating } from '@/components';
 import { Tag } from '@/components/Tag/Tag';
 import { withLayout } from '@/layout/Layout';
+import { GetStaticProps } from 'next';
 import { Noto_Sans_KR } from 'next/font/google';
 import { useEffect, useState } from 'react';
+import axios from 'axios';
+import { MenuItem } from '@/interfaces/menu.interface';
 
 const notoSans = Noto_Sans_KR({ subsets: ['cyrillic'] });
 
-function Home() {
+function Home({ menu, firstCategory }: HomeProps) {
   const [counter, setCounter] = useState<number>(0);
   useEffect(() => {
-    console.log(`Counter: ${counter}`);
     return function cleanUp() {
-      console.log('UnMount');
     }
   }, [counter])
   return (
@@ -31,8 +32,24 @@ function Home() {
       <Tag size='sm' color='green' >Green</Tag>
       <Tag size='sm' color='primary' href='/about'>Primary</Tag>
       <Rating rating={counter} isEditable={true} setRating={setCounter} />
+      <ul>
+        {menu.map(m => (<li key={m._id.secondCategory}>{m._id.secondCategory}</li>))}
+      </ul>
     </main>
   );
 }
 
 export default withLayout(Home);
+
+export const getStaticProps: GetStaticProps = async () => {
+  const firstCategory = 0;
+  const { data: menu } = await axios.post<MenuItem[]>(process.env.NEXT_PUBLIC_DOMAIN + '/api/top-page/find', {firstCategory});
+  return {
+    props: { menu, firstCategory }
+  };
+};
+
+interface HomeProps extends Record<string, unknown> {
+  menu: MenuItem[],
+  firstCategory: number
+}
--- top-app-page-router/top-app/pages/test/index.tsx
+++ top-app-page-router/top-app/pages/test/index.tsx
@@ -1,10 +1,16 @@
+import { User } from '@/interfaces/user.test.interface';
 import styles from './style.module.css';
+import axios from 'axios';
 
-export default function TestPage(): JSX.Element {
+export default function TestPage({ users }: TestPageProps): JSX.Element {
     return (
         <div className={styles.container}>
             <div className={styles.header}>Header</div>
-            <div className={styles.main}>Main</div>
+            <div className={styles.main}>
+                <ul>
+                    {users.map(user => <li key={user.id}>{user.name} {user.phone}</li>)}
+                </ul>
+            </div>
             <div className={styles.sidebar}>Sidebar</div>
             <div className={styles.footer}>Footer</div>
         </div>
@@ -20,4 +26,15 @@ export default function TestPage(): JSX.Element {
         //     <div className={styles.c9}>9</div>
         // </div>
     );
+}
+
+export const getStaticProps = async () => {
+    const { data: users } = await axios.get<User[]>('https://jsonplaceholder.typicode.com/users');
+    return {
+        props: { users }
+    }
+}
+
+interface TestPageProps {
+    users: User[]
 }
