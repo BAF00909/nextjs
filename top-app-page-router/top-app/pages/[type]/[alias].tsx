ef00b6b4bc5217833a0b874385a7929eafbcc5d6
Alexander Breus <lis00909@gmail.com>
2024-05-21 22:03:47 +0300

    add meta and og tags

--- top-app-page-router/top-app/pages/[type]/[alias].tsx
+++ top-app-page-router/top-app/pages/[type]/[alias].tsx
@@ -8,62 +8,74 @@ import { ProductModel } from "@/interfaces/product.interface";
 import { firstLevelMenu } from "@/helpers/helpers";
 import { TopPageComponent } from "@/page-components/TopPageComponent/TopPageComponent";
 import { API } from "@/helpers/api";
+import Head from "next/head";
 
 function TopPage({ menu, page, products, firstCategory }: TopPageProps) {
-    if (!page || !products) {
-        return <></>;
-    }
-    return <TopPageComponent firstCategory={firstCategory} page={page} products={products} />;
+	if (!page || !products) {
+		return <></>;
+	}
+	return (
+		<>
+			<Head>
+				<title>{page.metaTitle}</title>
+				<meta name="description" content={page.metaDescription} />
+				<meta property="og:title" content={page.metaTitle} />
+				<meta property="og:description" content={page.metaDescription} />
+				<meta property="og:type" content={'article'} />
+			</Head>
+			<TopPageComponent firstCategory={firstCategory} page={page} products={products} />
+		</>
+	);
 }
 
 export default withLayout(TopPage);
 
 export const getStaticPaths: GetStaticPaths = async () => {
-    let paths: string[] = [];
-    for (const m of firstLevelMenu) {
-        const { data: menu } = await axios.post<MenuItem[]>(API.topPage.find, { firstCategory: m.id });
-        paths.concat(menu.flatMap(s => s.pages.map(p => `/${m.route}/${p.alias}`)));
-    }
-    return {
-        paths: paths,
-        fallback: true
-    }
+	let paths: string[] = [];
+	for (const m of firstLevelMenu) {
+		const { data: menu } = await axios.post<MenuItem[]>(API.topPage.find, { firstCategory: m.id });
+		paths.concat(menu.flatMap(s => s.pages.map(p => `/${m.route}/${p.alias}`)));
+	}
+	return {
+		paths: paths,
+		fallback: true
+	}
 }
 
 export const getStaticProps: GetStaticProps<TopPageProps> = async ({ params }: GetStaticPropsContext<ParsedUrlQuery>) => {
-    if (!params) {
-        return {
-            notFound: true,
-        }
-    }
-    try {
-        const firstCategoryItem = firstLevelMenu.find(m => m.route === params.type);
-        if (!firstCategoryItem) {
-            return {
-                notFound: true,
-            }
-        }
-        const { data: menu } = await axios.post<MenuItem[]>(API.topPage.find, { firstCategory: firstCategoryItem?.id });
-        if (!menu.length) {
-            return {
-                notFound: true
-            };
-        }
-        const { data: page } = await axios.get<TopPageModel>(API.topPage.byAlias + params.alias);
-        const { data: products } = await axios.post<ProductModel[]>(API.topPage.productFind, { category: page.category, limit: 10 });
-        return {
-            props: { menu, firstCategory: firstCategoryItem?.id, page, products }
-        };
-    } catch (e) {
-        return {
-            notFound: true
-        };
-    }
+	if (!params) {
+		return {
+			notFound: true,
+		}
+	}
+	try {
+		const firstCategoryItem = firstLevelMenu.find(m => m.route === params.type);
+		if (!firstCategoryItem) {
+			return {
+				notFound: true,
+			}
+		}
+		const { data: menu } = await axios.post<MenuItem[]>(API.topPage.find, { firstCategory: firstCategoryItem?.id });
+		if (!menu.length) {
+			return {
+				notFound: true
+			};
+		}
+		const { data: page } = await axios.get<TopPageModel>(API.topPage.byAlias + params.alias);
+		const { data: products } = await axios.post<ProductModel[]>(API.topPage.productFind, { category: page.category, limit: 10 });
+		return {
+			props: { menu, firstCategory: firstCategoryItem?.id, page, products }
+		};
+	} catch (e) {
+		return {
+			notFound: true
+		};
+	}
 }
 
 interface TopPageProps extends Record<string, unknown> {
-    menu: MenuItem[],
-    firstCategory: TopLevelCategory,
-    page: TopPageModel,
-    products: ProductModel[]
+	menu: MenuItem[],
+	firstCategory: TopLevelCategory,
+	page: TopPageModel,
+	products: ProductModel[]
 }
--- top-app-page-router/top-app/pages/index.tsx
+++ top-app-page-router/top-app/pages/index.tsx
@@ -7,49 +7,53 @@ import { useEffect, useState } from 'react';
 import axios from 'axios';
 import { MenuItem } from '@/interfaces/menu.interface';
 import { API } from '@/helpers/api';
+import { notFound } from 'next/navigation';
 
 const notoSans = Noto_Sans_KR({ subsets: ['cyrillic'] });
 
 function Home({ menu, firstCategory }: HomeProps) {
-  const [counter, setCounter] = useState<number>(0);
-  useEffect(() => {
-    return function cleanUp() {
-    }
-  }, [counter])
-  return (
-    <main className={`${notoSans.className}`}>
-      <Htag tag='h1'>Заголовок 1</Htag>
-      <Htag tag='h2'>Заголовок 2</Htag>
-      <Htag tag='h3'>Заголовок 3</Htag>
-      <Button appearence='primary' onClick={() => setCounter(count => count + 1)} arrow='right'>Primary</Button>
-      <Button appearence='ghost' arrow='right'>Ghost</Button>
-      <P>Без размера</P>
-      <P size={'sm'}>Маленьктй текст</P>
-      <P size={'m'}>Средний текст</P>
-      <P size={'lg'}>Большой текст</P>
-      <Tag size='sm' color='ghost' >Ghost</Tag>
-      <Tag size='sm' color='red' >Red</Tag>
-      <Tag size='lg' color='gray' >{counter}</Tag>
-      <Tag size='sm' color='green' >Green</Tag>
-      <Tag size='sm' color='primary' href='/about'>Primary</Tag>
-      <Rating rating={counter} isEditable={true} setRating={setCounter} />
-      <Input placeholder='Имя' />
-      <TextArea placeholder='Текст отзыва' />
-    </main>
-  );
+	const [counter, setCounter] = useState<number>(0);
+	useEffect(() => {
+		return function cleanUp() {
+		}
+	}, [counter])
+	return (
+		<main className={`${notoSans.className}`}>
+			<Htag tag='h1'>Заголовок 1</Htag>
+			<Htag tag='h2'>Заголовок 2</Htag>
+			<Htag tag='h3'>Заголовок 3</Htag>
+			<Button appearence='primary' onClick={() => setCounter(count => count + 1)} arrow='right'>Primary</Button>
+			<Button appearence='ghost' arrow='right'>Ghost</Button>
+			<P>Без размера</P>
+			<P size={'sm'}>Маленьктй текст</P>
+			<P size={'m'}>Средний текст</P>
+			<P size={'lg'}>Большой текст</P>
+			<Tag size='sm' color='ghost' >Ghost</Tag>
+			<Tag size='sm' color='red' >Red</Tag>
+			<Tag size='lg' color='gray' >{counter}</Tag>
+			<Tag size='sm' color='green' >Green</Tag>
+			<Tag size='sm' color='primary' href='/about'>Primary</Tag>
+			<Rating rating={counter} isEditable={true} setRating={setCounter} />
+			<Input placeholder='Имя' />
+			<TextArea placeholder='Текст отзыва' />
+		</main>
+	);
 }
 
 export default withLayout(Home);
 
 export const getStaticProps: GetStaticProps = async () => {
-  const firstCategory = 0;
-  const { data: menu } = await axios.post<MenuItem[]>(API.topPage.find, {firstCategory});
-  return {
-    props: { menu, firstCategory }
-  };
+	const firstCategory = 0;
+	const { data: menu } = await axios.post<MenuItem[]>(API.topPage.find, { firstCategory });
+	if (!menu) {
+		return { notFound: true };
+	}
+	return {
+		props: { menu, firstCategory }
+	};
 };
 
 interface HomeProps extends Record<string, unknown> {
-  menu: MenuItem[],
-  firstCategory: number
+	menu: MenuItem[],
+	firstCategory: number
 }
